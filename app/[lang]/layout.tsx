3fc79c8d0e6f985f6f1475b2b9d5d1f7c86c86c7
alvaldes <angelluis2605@gmail.com>
2025-01-24 09:37:02 -0600

    Actualizar metadatos de Open Graph: cambiar URLs de imágenes a rutas locales

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -12,13 +12,14 @@ import Navbar from './components/Nav/Navbar'
 export const metadata: Metadata = {
   title: 'Angel L. Valdés',
   description: 'Software engineer with +3 years of web development expertise',
+  metadataBase: new URL('https://portfolio-7swh468na-alvaldes-projects.vercel.app'),
   openGraph: {
     title: 'Angel L. Valdés',
     description: 'Software engineer with +3 years of web development expertise',
     url: 'https://alvaldes.vercel.app/en',
     images: [
       {
-        url: 'https://opengraph.b-cdn.net/production/images/adae5c0f-24ed-45c9-94ef-cf1dec6897d9.jpg?token=YqmFuWeeLHb7eVlIUIZVv1QX6C48KjMXZqI_odyCPoo&height=630&width=1200&expires=33273686478',
+        url: '/images/og-image.jpg',
       },
     ],
     locale: 'en_US',
@@ -28,7 +29,7 @@ export const metadata: Metadata = {
     card: "summary_large_image", 
     site: "@site", 
     creator: "@creator", 
-    images: "https://opengraph.b-cdn.net/production/images/adae5c0f-24ed-45c9-94ef-cf1dec6897d9.jpg?token=YqmFuWeeLHb7eVlIUIZVv1QX6C48KjMXZqI_odyCPoo&height=630&width=1200&expires=33273686478"
+    images: '/images/og-image.jpg',
   },
 }
 
